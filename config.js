b2fe4047b86ec0cc617a1dbe7fdbdb98e8829773
firelord69 <79193827+firelord69@users.noreply.github.com>
2021-11-09 13:05:47 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAhurtEar9trKnFFJ2kiNSZyZdLLb9buYs",
+    authDomain: "storytelling-app-39e60.firebaseapp.com",
+    projectId: "storytelling-app-39e60",
+    storageBucket: "storytelling-app-39e60.appspot.com",
+    messagingSenderId: "980922453240",
+    appId: "1:980922453240:web:7e7832a085fc888866bf0d",
+    measurementId: "G-G7JT4CQKLE"
+  };
